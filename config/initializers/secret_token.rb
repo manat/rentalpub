77ba4df2baa57cb1f5a1c764ad7506a1284a3b44
Manat Lertpeetipat <mlertpee@gmail.com>
2012-07-13 23:15:05 +0700

    Init project with Rails 3.2.6

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rentalpub::Application.config.secret_token = '6381724efd3a9039cce96f602636d3c67e9bcf72de1ec1d5d919440f2f537d9a2e811593c6b191ba414d53534e83476456aeca0f762b036311d6ed546cae6c03'
